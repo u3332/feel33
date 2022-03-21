16c4a5395dbf245d5cf40f49ff8553eb0ba7fa49
u3332 <102060129+u3332@users.noreply.github.com>
2022-03-21 19:16:36 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,27 @@
+var isValidNewBlock = (newBlock, previousBlock) => {
+
+if (previousBlock.index + 1 !== newBlock.index) {
+
+console.log('invalid index');
+
+return false;
+
+} else if (previousBlock.hash !== newBlock.previousHash) {
+
+console.log('invalid previoushash');
+
+return false;
+
+} else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+
+console.log(typeof (newBlock.hash) + ' ' + typeof calculateHashForBlock(newBlock));
+
+console.log('invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+
+return false;
+
+}
+
+return true;
+
+};
